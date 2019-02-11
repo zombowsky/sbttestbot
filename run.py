961458e62d31007f90fb8333e904562e509690ad
dmitry <dmitry@dmitry.com>
2019-02-11 20:35:51 +0300

    chains, stt

--- run.py
+++ run.py
@@ -1,22 +1,29 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import settings
 import logging
-from tasks import find_artist_info
+from tasks import find_artist_info, send_message, download_voice, recognize_speech
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 
 
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="test hello")
-
-
+    send_message.delay(token=settings.tg_token, chat_id=update.message.chat_id, msg="test hello")
 
 
 def search(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="start searching...")
+    logging.debug("start searching...")
     query = ' '.join(update.message.text.split()[1:])
-    ct = find_artist_info.delay(query)
-    bot.send_message(chat_id=update.message.chat_id, text=ct.get())
+    ct = find_artist_info.s(query) | send_message.s(token=settings.tg_token, chat_id=update.message.chat_id)
+    ct()
+
+
+def voice_search(bot, update):
+    logging.debug("start searching voice...")
+    ct = download_voice.s(settings.tg_token, update.message.voice.file_id) | \
+         recognize_speech.s() | \
+         find_artist_info.s() | \
+         send_message.s(token=settings.tg_token, chat_id=update.message.chat_id)
+    ct()
 
 
 updater = Updater(settings.tg_token)
@@ -24,5 +31,5 @@ dispatcher = updater.dispatcher
 
 dispatcher.add_handler(CommandHandler('start', start))
 dispatcher.add_handler(CommandHandler('search', search))
-
+dispatcher.add_handler(MessageHandler(Filters.voice, voice_search))
 updater.start_polling()
--- tasks.py
+++ tasks.py
@@ -1,8 +1,9 @@
 from celery import Celery
+from telegram import Bot
 import requests
 import settings
 import json
-
+from watson_developer_cloud import SpeechToTextV1
 
 app = Celery('tasks', backend='amqp', broker='amqp://')
 
@@ -15,3 +16,22 @@ def find_artist_info(artist):
         )).text
     text = json.loads(result)["artist"]["bio"]["summary"].split('<a')[0]
     return text
+
+
+@app.task()
+def send_message(msg, token, chat_id):
+    Bot(token).send_message(chat_id=chat_id, text=msg)
+
+
+@app.task()
+def download_voice(token, file_id):
+    Bot(token).get_file(file_id).download(file_id+'.ogg')
+    return file_id+'.ogg'
+
+
+@app.task()
+def recognize_speech(filename):
+    print(filename)
+    speech_to_text = SpeechToTextV1(iam_apikey=settings.ibm_api_key, url=settings.ibm_api)
+    result = speech_to_text.recognize(audio=open(filename, 'rb'), content_type='audio/ogg').get_result()
+    return result["results"][0]["alternatives"][0]["transcript"].strip()
